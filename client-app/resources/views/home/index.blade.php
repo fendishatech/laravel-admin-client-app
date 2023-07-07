9ab307e932b39ec1a542eff1f5be985906d68978
fendishatech <liyukibir@gmail.com>
2023-07-07 08:37:02 -0700

    client: auth

--- client-app/resources/views/home/index.blade.php
+++ client-app/resources/views/home/index.blade.php
@@ -6,5 +6,7 @@
 
 
 @section('content')
-    <h1>Home Page with Sections</h1>
+    <div class="container mx-auto px-5">
+        <h1 class="text-2xl text-center">Home Page</h1>
+    </div>
 @endsection
