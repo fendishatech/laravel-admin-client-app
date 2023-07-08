61e6a700b33c3bd4a5b6be3b2722a384018bf54c
fendishatech <liyukibir@gmail.com>
2023-07-08 01:24:21 -0700

    admin: cleint and memeber migration and seeder

--- admin-panel/database/seeders/DatabaseSeeder.php
+++ admin-panel/database/seeders/DatabaseSeeder.php
@@ -12,6 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(UserSeeder::class);
+        // $this->call(UserSeeder::class);
+        // $this->call(ClientSeeder::class);
+        $this->call(MemberSeeder::class);
     }
 }
